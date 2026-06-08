0c7069d663328cbdedb8be606192dbfa11fa80aa
Aromal Satheesh <aromalsatheesh802003@gmail.com>
2026-06-08 20:10:57 +0700

    responsive

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -33,8 +33,8 @@ const Skills = () => {
           {/* Card 1 */}
           <div className="bg-slate-950 rounded-2xl p-6 md:p-10 md:col-span-2 h-72 cursor-pointer border-2 shadow-md hover:border-orange-300 transition-colors duration-200">
             <div className="flex justify-between text-gray-500">
-              <h1 className='lg:text-sm'>COURSES</h1>
-              <h1 className='bg-gray-900 rounded-2xl p-1'>40+ courses</h1>
+              <h1 className='text-sm'>COURSES</h1>
+              <h1 className='bg-gray-900 rounded-2xl p-1 text-sm '>40+ courses</h1>
             </div>
 
             <AiFillAndroid className="text-4xl text-blue-500 my-6" />
@@ -49,7 +49,7 @@ const Skills = () => {
               </h1>
             </div>
 
-            <div className='flex text-blue-500 font-medium md:text-normal mt-5'>
+            <div className='flex gap-2 text-blue-500 font-medium md:text-normal mt-5'>
               <a href="/">Explore</a>
               <FaArrowRight className='mt-1' />
             </div>
@@ -58,8 +58,8 @@ const Skills = () => {
           {/* Card 2 */}
           <div className="bg-slate-950 rounded-2xl p-6 md:p-10 h-72 cursor-pointer border-2 shadow-md hover:border-indigo-600 transition-colors duration-200">
             <div className="flex justify-between text-gray-500 font-normal text-sm">
-              <h1>COURSES</h1>
-              <h1 className='bg-gray-900 rounded-2xl p-1'>40+ courses</h1>
+              <h1 className='text-sm'>COURSES</h1>
+              <h1 className='bg-gray-900 rounded-2xl p-1 text-sm'>40+ courses</h1>
             </div>
 
             <LuBrain className="text-4xl text-violet-500 my-6" />
@@ -74,7 +74,7 @@ const Skills = () => {
               </h1>
             </div>
 
-            <div className='flex text-blue-500 font-medium md:text-normal mt-5'>
+            <div className='flex gap-2 text-blue-500 font-medium md:text-normal mt-5'>
               <a href="/">Explore</a>
               <FaArrowRight className='mt-1' />
             </div>
@@ -83,8 +83,8 @@ const Skills = () => {
           {/* Card 3 */}
           <div className="bg-slate-950 rounded-2xl p-6 md:p-10 h-72 cursor-pointer border-2 shadow-md hover:border-violet-600 transition-colors duration-200">
             <div className="flex justify-between text-gray-500">
-              <h1>COURSES</h1>
-              <h1 className='bg-gray-900 rounded-2xl p-1'>40+ courses</h1>
+              <h1 className='text-sm'>COURSES</h1>
+              <h1 className='bg-gray-900 rounded-2xl p-1 text-sm'>40+ courses</h1>
             </div>
 
             <SiDjango className="text-4xl text-green-400 my-6" />
@@ -99,7 +99,7 @@ const Skills = () => {
               </h1>
             </div>
 
-            <div className='flex text-blue-500 font-medium md:text-normal mt-5'>
+            <div className='flex gap-2 text-blue-500 font-medium md:text-normal mt-5'>
               <a href="/">Explore</a>
               <FaArrowRight className='mt-1' />
             </div>
@@ -108,8 +108,8 @@ const Skills = () => {
           {/* Card 4 */}
           <div className="bg-slate-950 rounded-2xl p-6 md:p-10 md:col-span-2 h-72 cursor-pointer border-2 shadow-md hover:border-green-600 transition-colors duration-200">
             <div className="flex justify-between text-gray-500">
-              <h1>COURSES</h1>
-              <h1 className='bg-gray-900 rounded-2xl p-1'>40+ courses</h1>
+              <h1 className='text-sm'>COURSES</h1>
+              <h1 className='bg-gray-900 rounded-2xl p-1 text-sm'>40+ courses</h1>
             </div>
 
             <SiFastapi className="text-4xl text-orange-500 my-6" />
@@ -124,7 +124,7 @@ const Skills = () => {
               </h1>
             </div>
 
-            <div className='flex text-blue-500 font-medium md:text-normal mt-5'>
+            <div className='flex gap-2 text-blue-500 font-medium md:text-normal mt-5'>
               <a href="/">Explore</a>
               <FaArrowRight className='mt-1' />
             </div>
